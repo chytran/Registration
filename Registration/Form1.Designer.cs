b6fd2aa0062481ce365246fb121ff4c33a981dc0
Kevin Tran <kevintran1142@gmail.com>
2022-03-20 13:42:44 -0700

    SQL included

--- Registration/Form1.Designer.cs
+++ Registration/Form1.Designer.cs
@@ -36,14 +36,15 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.addresstxt = new System.Windows.Forms.TextBox();
+            this.emailtxt = new System.Windows.Forms.TextBox();
+            this.usertxt = new System.Windows.Forms.TextBox();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.phonetxt = new System.Windows.Forms.TextBox();
+            this.passwordtxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.genderOption = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
@@ -51,142 +52,159 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(161, 86);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "First Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(161, 129);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 0;
-            this.label2.Text = "label1";
+            this.label2.Text = "Address";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(376, 86);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 0;
-            this.label3.Text = "label1";
+            this.label3.Text = "Last Name";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(376, 129);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(42, 13);
             this.label4.TabIndex = 0;
-            this.label4.Text = "label1";
+            this.label4.Text = "Gender";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(161, 209);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(32, 13);
             this.label5.TabIndex = 0;
-            this.label5.Text = "label1";
+            this.label5.Text = "Email";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(376, 209);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(38, 13);
             this.label6.TabIndex = 0;
-            this.label6.Text = "label1";
+            this.label6.Text = "Phone";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(161, 252);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(55, 13);
             this.label7.TabIndex = 0;
-            this.label7.Text = "label1";
+            this.label7.Text = "Username";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(376, 252);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.Size = new System.Drawing.Size(53, 13);
             this.label8.TabIndex = 0;
-            this.label8.Text = "label1";
+            this.label8.Text = "Password";
             // 
-            // textBox1
+            // txtFname
             // 
-            this.textBox1.Location = new System.Drawing.Point(221, 83);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtFname.Location = new System.Drawing.Point(221, 83);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(100, 20);
+            this.txtFname.TabIndex = 1;
             // 
-            // textBox2
+            // addresstxt
             // 
-            this.textBox2.Location = new System.Drawing.Point(221, 126);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
+            this.addresstxt.Location = new System.Drawing.Point(221, 126);
+            this.addresstxt.Name = "addresstxt";
+            this.addresstxt.Size = new System.Drawing.Size(100, 20);
+            this.addresstxt.TabIndex = 1;
             // 
-            // textBox3
+            // emailtxt
             // 
-            this.textBox3.Location = new System.Drawing.Point(221, 206);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 1;
+            this.emailtxt.Location = new System.Drawing.Point(221, 206);
+            this.emailtxt.Name = "emailtxt";
+            this.emailtxt.Size = new System.Drawing.Size(100, 20);
+            this.emailtxt.TabIndex = 1;
             // 
-            // textBox4
+            // usertxt
             // 
-            this.textBox4.Location = new System.Drawing.Point(221, 249);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 1;
+            this.usertxt.Location = new System.Drawing.Point(221, 249);
+            this.usertxt.Name = "usertxt";
+            this.usertxt.Size = new System.Drawing.Size(100, 20);
+            this.usertxt.TabIndex = 1;
             // 
-            // textBox5
+            // txtLname
             // 
-            this.textBox5.Location = new System.Drawing.Point(446, 83);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 1;
+            this.txtLname.Location = new System.Drawing.Point(446, 83);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(100, 20);
+            this.txtLname.TabIndex = 1;
             // 
-            // textBox6
+            // phonetxt
             // 
-            this.textBox6.Location = new System.Drawing.Point(446, 126);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 1;
+            this.phonetxt.Location = new System.Drawing.Point(446, 206);
+            this.phonetxt.Name = "phonetxt";
+            this.phonetxt.Size = new System.Drawing.Size(100, 20);
+            this.phonetxt.TabIndex = 1;
             // 
-            // textBox7
+            // passwordtxt
             // 
-            this.textBox7.Location = new System.Drawing.Point(446, 206);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 20);
-            this.textBox7.TabIndex = 1;
+            this.passwordtxt.Location = new System.Drawing.Point(446, 249);
+            this.passwordtxt.Name = "passwordtxt";
+            this.passwordtxt.PasswordChar = '*';
+            this.passwordtxt.Size = new System.Drawing.Size(100, 20);
+            this.passwordtxt.TabIndex = 1;
             // 
-            // textBox8
+            // button1
             // 
-            this.textBox8.Location = new System.Drawing.Point(446, 249);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 20);
-            this.textBox8.TabIndex = 1;
+            this.button1.Location = new System.Drawing.Point(470, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // genderOption
+            // 
+            this.genderOption.FormattingEnabled = true;
+            this.genderOption.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Others"});
+            this.genderOption.Location = new System.Drawing.Point(446, 129);
+            this.genderOption.Name = "genderOption";
+            this.genderOption.Size = new System.Drawing.Size(121, 21);
+            this.genderOption.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.genderOption);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passwordtxt);
+            this.Controls.Add(this.usertxt);
+            this.Controls.Add(this.phonetxt);
+            this.Controls.Add(this.emailtxt);
+            this.Controls.Add(this.txtLname);
+            this.Controls.Add(this.addresstxt);
+            this.Controls.Add(this.txtFname);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label4);
@@ -212,14 +230,15 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.TextBox addresstxt;
+        private System.Windows.Forms.TextBox emailtxt;
+        private System.Windows.Forms.TextBox usertxt;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.TextBox phonetxt;
+        private System.Windows.Forms.TextBox passwordtxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox genderOption;
     }
 }
 
--- Registration/Form1.cs
+++ Registration/Form1.cs
@@ -7,16 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Registration
 {
     public partial class Form1 : Form
     {
+        private const string ConnectionString = (@"Data Source=DESKTOP-E93ERMF\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConnectionString);
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[register]
+           ([firstname]
+           ,[lastname]
+           ,[address]
+           ,[gender]
+           ,[email]
+           ,[phone]
+           ,[username]
+           ,[password])
+     VALUES
+           ('" + txtFname.Text + "', '" + txtLname.Text + "', '" + addresstxt.Text + "', '" + genderOption.SelectedItem.ToString() + "', '" + emailtxt.Text + "', '" + phonetxt.Text + "', '" + usertxt.Text + "', '" + passwordtxt.Text +"')" , con);
+           con.Open();  
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Register Successful");
+        }
     }
 }
